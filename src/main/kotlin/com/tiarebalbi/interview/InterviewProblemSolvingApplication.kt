1c44204c70885aff7adfa7b781007c0eefe6b4b7
Tiarê Balbi Bonamini <me@tiarebalbi.com>
2018-11-07 05:43:32 +0000

    Initial commit

---
+++ src/main/kotlin/com/tiarebalbi/interview/InterviewProblemSolvingApplication.kt
@@ -0,0 +1,11 @@
+package com.tiarebalbi.interview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterviewProblemSolvingApplication
+
+fun main(args: Array<String>) {
+    runApplication<InterviewProblemSolvingApplication>(*args)
+}
