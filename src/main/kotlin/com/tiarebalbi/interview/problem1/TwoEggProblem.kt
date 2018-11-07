b33d17949d537e0c91893a9ac6384d6fd14fb714
Tiarê Balbi Bonamini <me@tiarebalbi.com>
2018-11-07 08:11:32 +0000

    Refactor: Removing duplicated attribute in the search method
    Signed-off-by: Tiarê Balbi Bonamini <me@tiarebalbi.com>

--- src/main/kotlin/com/tiarebalbi/interview/problem1/TwoEggProblem.kt
+++ src/main/kotlin/com/tiarebalbi/interview/problem1/TwoEggProblem.kt
@@ -24,7 +24,7 @@ class TwoEggProblem constructor(
   private val secondEgg: MutableList<Int> = mutableListOf()
 
   fun whichFloorIsSafeToDropTheEgg(): Int {
-    val result = search(floors, theSafeFloorToDrop, 1, numberOfFloors)
+    val result = search(floors, 1, numberOfFloors)
 
     logger.info("Highest floor an egg won't break from: $result")
     logger.info("Floors we drop first egg from: $firstEgg")
@@ -37,12 +37,11 @@ class TwoEggProblem constructor(
 
   private fun search(
     data: List<Int>,
-    safeFloorToDrop: Int,
     lowestNumber: Int,
     highestNumber: Int,
     brokenTheEgg: Boolean = false
   ): Int {
-    if (highestNumber < lowestNumber || safeFloorToDrop > highestNumber) return -1
+    if (highestNumber < lowestNumber || theSafeFloorToDrop > highestNumber) return -1
 
     val middleOrHighestNumber = if (brokenTheEgg) highestNumber else (lowestNumber + highestNumber) / 2
 
@@ -55,13 +54,13 @@ class TwoEggProblem constructor(
     }
 
     return when {
-      safeFloorToDrop == data[valueToCompareAgainst] -> {
+      theSafeFloorToDrop == data[valueToCompareAgainst] -> {
         data[valueToCompareAgainst]
       }
-      (safeFloorToDrop > data[valueToCompareAgainst]) && !brokenTheEgg -> search(data, safeFloorToDrop, middleOrHighestNumber + 1, highestNumber)
+      (theSafeFloorToDrop > data[valueToCompareAgainst]) && !brokenTheEgg -> search(data, middleOrHighestNumber + 1, highestNumber)
       else -> {
         val newLowest = if (brokenTheEgg) lowestNumber + 1 else lowestNumber
-        search(data, safeFloorToDrop, newLowest, middleOrHighestNumber, true)
+        search(data, newLowest, middleOrHighestNumber, true)
       }
     }
   }
